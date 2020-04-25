8ed9cc085258ebeb0b82f3e0eaf1a03ddd025e80
Gianfranco Manrique <gmanrique.422@gmail.com>
2020-04-25 13:32:38 -0500

    Archivos semana 03

---
+++ SEM-03/CargaAsignacion.sql
@@ -0,0 +1,86 @@
+--Carga de Asignación
+
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (2,11,1,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (2,12,1,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (2,13,1,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (3,14,1,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (3,15,2,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (3,16,2,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (4,17,2,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (4,18,2,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (4,19,3,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (5,20,3,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (5,21,3,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (5,22,3,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (7,23,4,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (7,24,4,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (7,25,4,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (8,26,4,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (8,27,5,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (8,28,5,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (9,29,5,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (9,30,5,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (9,31,6,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (10,32,6,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (10,33,6,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (10,34,6,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (11,35,7,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (11,36,7,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (11,37,7,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (12,38,7,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (12,39,8,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (12,40,8,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (13,41,8,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (13,42,8,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (13,43,9,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (14,44,9,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (14,45,9,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (14,46,9,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (16,47,10,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (16,48,10,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (16,11,10,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (16,12,10,'2020-01-19',NULL,getdate(),null)
+GO 
+INSERT INTO Asignacion(idmanzana,idencuestador,idsupervisor,fecinicio,fecfin,fecregistro,fecultmodifica) values (0,0,0,'2020-01-20',NULL,getdate(),null)
+GO 
+
+select * from Asignacion
