389d6e381177776f8390ebcdd456021b7b418555
Gianfranco Manrique <gmanrique.422@gmail.com>
2020-05-02 10:08:30 -0500

    Archivos semana 03 parte 02

--- SEM-03/Parte02.sql
+++ SEM-03/Parte02.sql
@@ -15,3 +15,19 @@ inner join Padron ps on s.idpadron=ps.idpadron
 where convert(varchar(8),getdate(),112) between convert(varchar(8),a.fecinicio,112) and 
 convert(varchar(8),isnull(a.fecfin,getdate()),112)
 order by MANZANA 
+
+--03.06
+--Tabla izquierda: Trabajador (Prevalece)
+--Tabla derecha: Asignacion
+
+select t.usuario as USUARIO,t.idtrabajador as [ID ENCUESTADOR],isnull(a.idmanzana,0) as [ID MANZANA],
+isnull(fecinicio,'9999-12-31') as FECINICIO,isnull(fecfin,'9999-12-31') as FECFIN
+from Trabajador t left join Asignacion a on t.idtrabajador=a.idencuestador --and a.idmanzana=4
+where t.tipo='E'
+
+select t.usuario as USUARIO,t.idtrabajador as [ID ENCUESTADOR],isnull(a.idmanzana,0) as [ID MANZANA],
+isnull(fecinicio,'9999-12-31') as FECINICIO,isnull(fecfin,'9999-12-31') as FECFIN
+from Trabajador t left join Asignacion a on t.idtrabajador=a.idencuestador 
+where t.tipo='E' --and a.idmanzana=4
+
+--03.07
