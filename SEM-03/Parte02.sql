6fe41397a681adf1b5bf2bdf35399430a521785d
Gianfranco Manrique <gmanrique.422@gmail.com>
2020-05-02 09:32:03 -0500

    Archivos semana 03 parte 02

---
+++ SEM-03/Parte02.sql
@@ -0,0 +1,17 @@
+--03.05
+select m.nombre as MANZANA,ltrim(pe.nombres)+' '+ltrim(pe.apellidos) as ENCUESTADOR,
+ltrim(ps.nombres)+' '+ltrim(ps.apellidos) as SUPERVISOR,convert(varchar(8),a.fecinicio,112) as FECINICIO,
+convert(varchar(8),getdate(),112) as HOY,
+convert(varchar(8),isnull(a.fecfin,getdate()),112) as FECFIN
+from Asignacion a 
+--MANZANA
+inner join Manzana m on a.idmanzana=m.idmanzana
+--ENCUESTADOR
+inner join Trabajador e on a.idencuestador=e.idtrabajador and e.tipo='E'
+inner join Padron pe on e.idpadron=pe.idpadron
+--SUPERVISOR
+inner join Trabajador s on a.idsupervisor=s.idtrabajador and s.tipo='S'
+inner join Padron ps on s.idpadron=ps.idpadron
+where convert(varchar(8),getdate(),112) between convert(varchar(8),a.fecinicio,112) and 
+convert(varchar(8),isnull(a.fecfin,getdate()),112)
+order by MANZANA 
