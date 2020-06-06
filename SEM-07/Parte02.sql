52f0f09b853d6d6fbe52b68fe63f480a0e500237
Gianfranco Manrique <gmanrique.422@gmail.com>
2020-06-06 11:39:09 -0500

    Archivos semana 07 parte 02

--- SEM-07/Parte02.sql
+++ SEM-07/Parte02.sql
@@ -156,3 +156,122 @@ execute usp_actualiza_asignacion @idencuestador=21,@idmanzana=5,@fecinicio='2020
 --ACTUALIZACIÓN CON ERROR
 execute usp_actualiza_asignacion @idencuestador=1000,@idmanzana=5,@fecinicio='2020-07-01',
 @fecfin='2020-12-31',@idsupervisor=4
+
+--07.08
+alter table TipoDocumento add categoria varchar(1)
+
+update TipoDocumento
+set categoria='P' where idtipo in (1,2,4,5,6)
+
+update TipoDocumento
+set categoria='E' where idtipo=3
+
+select * from TipoDocumento
+
+alter procedure usp_actualiza_padron 
+	(
+	@idtipo int,@numdoc varchar(15),
+	@nombres varchar(50), @apellidos varchar(40), @sexo varchar(1),
+	@fecnacimiento date, @direccion varchar(300),@idubigeo int,@razon_social varchar(50)
+	)
+	as
+	begin
+		declare @mensaje varchar(500)
+
+		if exists(select 1 from Padron p inner join TipoDocumento t on p.idtipo=t.idtipo
+		where t.categoria='P' and p.idtipo=@idtipo and numdoc=@numdoc)--Si existe categoría 'P' para el tipo de documento y por número de documento
+
+		begin 
+			update p
+			set 
+				   p.nombres=@nombres,
+				   p.apellidos=@apellidos,
+				   p.sexo=@sexo,
+				   p.fecnacimiento=@fecnacimiento,
+				   p.direccion=@direccion,
+				   p.idubigeo=@idubigeo
+			from  Padron p
+			where idtipo=@idtipo and numdoc=@numdoc 
+
+			set @mensaje='Persona actualizada'
+		end
+		
+		if exists(select 1 from Padron p inner join TipoDocumento t on p.idtipo=t.idtipo
+		where t.categoria='E' and p.idtipo=@idtipo and numdoc=@numdoc)--Si existe categoría 'E' para el tipo de documento y por número de documento
+		begin
+
+			update p
+			set    p.razon_social=@razon_social,
+			       p.direccion=@direccion,
+				   p.idubigeo=@idubigeo
+			from  Padron p
+			where idtipo=@idtipo and numdoc=@numdoc 
+
+			set @mensaje='Empresa actualizada'
+		end
+
+		if not exists(select 1 from Padron p inner join TipoDocumento t on p.idtipo=t.idtipo
+		where p.idtipo=@idtipo and numdoc=@numdoc)--Si no existe por tipo de documento y número de documento
+		begin
+
+			set @mensaje='Padrón no identificado'
+
+		end
+		select @mensaje as MENSAJE, @idtipo as TIPO, @numdoc as NUMDOC
+	end
+
+--ACTUALIZACIÓN EXITOSA DE PERSONA
+
+--1	1	40249116	DABADU	MARCA LAURA	NULL	NULL	1970-01-01	CA. San Martín de porras S/n	1	2020-04-18 12:39:16.277
+execute usp_actualiza_padron @idtipo=1,@numdoc='40249116',@nombres='GIANFRANCO', @apellidos='MANRIQUE VALENTÍN', 
+@sexo='M',@fecnacimiento='1990-01-09', @direccion='URB. LOS CIPRESES M-24',@idubigeo=1,
+@razon_social=null
+
+select * from Padron where idpadron=1
+
+--ACTUALIZACIÓN EXITOSA DE EMPRESA
+
+update Padron set numdoc='20602275320',idtipo=3,nombres=NULL,apellidos=NULL,razon_social='DEV MASTER PERÚ',sexo=NULL
+where idpadron=2
+
+-- ACTUALIZACIÓN CON ERROR
+
+--2	3	20602275320	NULL	NULL	DEV MASTER PERÚ	NULL	1970-01-02	CA. FERMIN DEL CASTILLO 421-6	1	2020-04-18 12:39:16.277	1
+execute usp_actualiza_padron @idtipo=4,@numdoc='20602275320',@nombres=NULL, @apellidos=NULL, 
+@sexo=NULL,@fecnacimiento=NULL, @direccion='AV. BRASIL 840 BREÑA',@idubigeo=2,
+@razon_social='DEV MASTER ACADEMY'
+
+select * from Padron where idpadron=2
+
+--07.09
+
+create procedure usp_eliminar_unidad(@idunidaduso int)
+as
+begin
+	declare @mensaje varchar(300),@idunidadusodel int=0
+	
+	if exists(select 1 from UnidadUso where idunidaduso=@idunidaduso)
+	begin
+		delete UnidadUso where idunidaduso=@idunidaduso
+
+		set @mensaje='Unidad de uso eliminada'
+		set @idunidadusodel=@idunidaduso
+	end
+	else
+	begin
+		set @mensaje='Unidad de uso no eliminada'
+		set @idunidadusodel=0
+	end
+
+	select @mensaje as MENSAJE, @idunidadusodel as UNIDAD_USO_ELIMINADA
+end
+
+--ELIMINACIÓN EXITOSA
+
+execute usp_eliminar_unidad 1001
+select * from UnidadUso where idunidaduso=1001
+
+--ELIMINACIÓN CON ERROR
+
+execute usp_eliminar_unidad 100000
+select * from UnidadUso where idunidaduso=100000
